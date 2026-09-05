47744e0b157abc3a61d311bc0e7602957741237b
MaksutSahin <103602396+MaksutSahin@users.noreply.github.com>
2026-09-04 20:24:54 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,23 +18,27 @@ export const metadata: Metadata = {
   openGraph: {
     title: "Gülbanu ve Furkan Düğün Davetiyesi",
     description: "Varlığınızla günümüzü güzelleştirmeniz, mutluluğumuza ortak olmanız dileğiyle.",
-    url:'https://gulbanu-furkan.vercel.app/',
+    url: 'https://gulbanu-furkan.vercel.app/',
     siteName: 'G&F Düğün Davetiyesi',
     images: [{
-      url:'/davetiye-arkaplan.png',
+      url: 'https://gulbanu-furkan.vercel.app/davetiye-arkaplan.png',
       width: 1200,
       height: 630,
-      alt:'G&F Düğün Davetiyesi',
-    }, ],
-    locale:'tr_TR',
-    type:'website',
+      alt: 'G&F Düğün Davetiyesi',
+    }],
+    locale: 'tr_TR',
+    type: 'website',
   },  
 };
 
-export default function RootLayout({ children }: LayoutProps<"/">) {
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html
-      lang="en"
+      lang="tr"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
